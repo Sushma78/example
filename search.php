e4e2c15ba08c7119a092036738546fe9cad6c350
Sushma78 <sushmasharma2536@gmail.com>
2022-09-30 21:11:03 +0530

    search page added

--- search.php
+++ search.php
@@ -1,1 +1,35 @@
-<?php
+ <?php
+    include_once("includes/header.php");
+
+    ?>
+ <div class="textboxContainer">
+     <input type="text" class="searchInput" placeholder="Search For Something...">
+ </div>
+
+ <div class="results"></div>
+ <script>
+     $(function() {
+         var username = '<?php echo $userLoggedIn; ?>';
+         var timer;
+
+         $(".searchInput").keyup(function() {
+             clearTimeout(timer);
+
+             timer = setTimeout(function() {
+                 var val = $(".searchInput").val();
+
+                 if (val == "") {
+                     $(".results").html("");
+                 } else {
+                     $.post("ajax/getSearchResults.php", {
+                         term: val,
+                         username: username
+                     }, function(data) {
+                         $(".results").html(data);
+                     });
+                 }
+
+             }, 500);
+         })
+     })
+ </script>
